e679df645d730825faf34655944d2c7242108dc2
daniel <daniel@wolfdigitalmedia.ro>
2023-11-23 22:33:43 +0200

    create site

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "/globals.css";
+import Header from "@/components/Shared/Header";
+import Footer from "@/components/Shared/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Left",
+  description: "Descriere Left",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header />
+
+        {children}
+
+        <Footer />
+      </body>
+    </html>
+  );
+}
