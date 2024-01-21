b05c184ddb380ae771b07ebf293f1c0d66c9a33a
unknown <daniel@wolfdigitalmedia.ro>
2024-01-21 17:05:13 +0200

    seo test 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,16 +23,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <NextSeo
-        title="LEFT"
-        description="O inițiativă pentru consolidarea unei platforme civico-politice pentru însănătoșirea vieții sociale, politice și economice în perspectiva anului electoral 2024"
-        openGraph={{
-          url: "/analytics",
-          title: "Analize - LEFT",
-          siteName: "LEFT",
-        }}
-      />
       <body className={inter.className + "relative"}>
+        <NextSeo
+          title="LEFT"
+          description="O inițiativă pentru consolidarea unei platforme civico-politice pentru însănătoșirea vieții sociale, politice și economice în perspectiva anului electoral 2024"
+          openGraph={{
+            url: "/analytics",
+            title: "Analize - LEFT",
+            siteName: "LEFT",
+          }}
+        />
         <LoginBar />
         <Header />
 
